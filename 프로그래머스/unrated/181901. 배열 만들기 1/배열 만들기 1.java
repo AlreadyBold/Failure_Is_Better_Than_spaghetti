34ca50cf44feff42e49ec25c1b74d78269205009
AlreadyBold <101548340+AlreadyBold@users.noreply.github.com>
2023-05-24 15:04:45 +0900

    [unrated] Title: 배열 만들기 1, Time: 0.01 ms, Memory: 78.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.java
+++ 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.java
@@ -3,7 +3,6 @@ class Solution {
         // 갯수 구하기 
         int a = n/k;
         int[] answer = new int[a];
-        
         for(int i = 1 ; i<=a;i++){
             answer[i-1] = i*k;
         }
