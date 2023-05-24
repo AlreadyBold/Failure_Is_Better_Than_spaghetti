b0a4817c29a31abaadcb1966d3208e82771835ef
AlreadyBold <101548340+AlreadyBold@users.noreply.github.com>
2023-05-24 09:26:45 +0900

    Update 배열 만들기 1.java

--- 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.java
+++ 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.java
@@ -2,11 +2,6 @@ class Solution {
     public int[] solution(int n, int k) {
         // 갯수 구하기 
         int a = n/k;
-        /*if ( n % k == 0 ){
-            a = n/k;
-        } else {
-            a = (n/k)+1;
-        }*/
         int[] answer = new int[a];
         
         for(int i = 1 ; i<=a;i++){
